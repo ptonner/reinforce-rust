ec952edd1b04c81f4f66fd08cd1bc761529806b0
Peter Tonner <peter.tonner@proton.me>
2024-09-24 16:37:30 -0400

    move all traits into environment, add basic readme

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,26 @@
 use std::collections::HashSet;
 
-trait IState {
-    /// Type enumerating possible actions possible from all states
-    type Action;
-
-    fn available_actions(&self) -> HashSet<Self::Action>;
-}
-
 trait IEnvironment {
     type Action;
-    type State: IState;
+    type State;
 
-    /// The full dynamics of the environment: the probability of transitioning from state `from` to state `to`, taking action `take` and receiving rewards `with`.
+    fn available_actions(from: &Self::State) -> HashSet<Self::Action>;
+
+    /// The full dynamics of the environment: the probability of
+    /// transitioning from state `from` to state `to`, taking action
+    /// `take` and receiving rewards `with`.
     fn prob(from: &Self::State, take: &Self::Action, to: &Self::State, with: &f32) -> f32;
 
-    /// Calculates the probability of transitioning from state `from` to state `to`, taking action `take`.
+    /// Calculates the probability of transitioning from state `from`
+    /// to state `to`, taking action `take`.
     ///
-    /// This is formally calcuated as the marginal probability over different rewards values. If `rewards` is implemented, then this probability can be calculated exactly, and is provided through the trait's default implementation.
+    /// This is formally calcuated as the marginal probability over
+    /// different rewards values. If `rewards` is implemented, then
+    /// this probability can be calculated exactly, and is provided
+    /// through the trait's default implementation.
     ///
-    /// If `rewards` is not or cannot be implemented (e.g. for an infinite range), then this function must be defined explitly
+    /// If `rewards` is not or cannot be implemented (e.g. for an
+    /// infinite range), then this function must be defined explitly
     fn prob_transition(from: &Self::State, take: &Self::Action, to: &Self::State) -> Option<f32> {
         let rewards = Self::rewards();
         if rewards.len() > 0 {
@@ -28,7 +30,10 @@ trait IEnvironment {
         }
     }
 
-    /// Enumerates the possible reward values in the environment (optional)
+    // fn expected_reward(from: &Self::State, )
+
+    /// Enumerates the possible reward values in the environment
+    /// (optional)
     fn rewards() -> Vec<f32> {
         vec![]
     }
@@ -37,26 +42,26 @@ trait IEnvironment {
 // Example interface implementations
 #[derive(Hash, Eq, PartialEq, Debug)]
 enum DoNothing {
-    nothing,
+    Nothing,
 }
 #[derive(Debug)]
-struct State;
-impl IState for State {
-    type Action = DoNothing;
-    fn available_actions(&self) -> HashSet<Self::Action> {
-        HashSet::from_iter(vec![DoNothing::nothing])
-    }
+enum Always {
+    Same,
 }
+
 #[derive(Debug)]
 struct Dull;
 impl IEnvironment for Dull {
-    type State = State;
+    type State = Always;
     type Action = DoNothing;
 
+    fn available_actions(from: &Self::State) -> HashSet<Self::Action> {
+        HashSet::from_iter(vec![DoNothing::Nothing])
+    }
     fn rewards() -> Vec<f32> {
         vec![0.0]
     }
-    fn prob(from: &Self::State, take: &Self::Action, to: &Self::State, with: &f32) -> f32 {
+    fn prob(_: &Self::State, _: &Self::Action, _: &Self::State, with: &f32) -> f32 {
         if *with == 0.0 {
             1.0
         } else {
@@ -66,18 +71,18 @@ impl IEnvironment for Dull {
 }
 
 fn main() {
-    let init = State {};
-    println!("Available actions: {:?}", init.available_actions());
+    let init = Always::Same;
+    println!("Available actions: {:?}", Dull::available_actions(&init));
     println!(
         "Prob: {:?}",
-        Dull::prob(&init, &DoNothing::nothing, &init, &0.0)
+        Dull::prob(&init, &DoNothing::Nothing, &init, &0.0)
     );
     println!(
         "Prob: {:?}",
-        Dull::prob(&init, &DoNothing::nothing, &init, &1.0)
+        Dull::prob(&init, &DoNothing::Nothing, &init, &1.0)
     );
     println!(
         "Transition prob: {:?}",
-        Dull::prob_transition(&init, &DoNothing::nothing, &init)
+        Dull::prob_transition(&init, &DoNothing::Nothing, &init)
     )
 }
